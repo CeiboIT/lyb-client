f689004c58b669d8ff9aa3f5afd1b98411d84e7a
sebastian <sebastian.fondra@gmail.com>
2015-06-02 19:08:05 -0300

    node server for demo pourpose

--- app.js
+++ app.js
@@ -6,19 +6,7 @@
 	var app = express();
 	var proxy = httpProxy.createProxyServer({});
 	
-	// var http = require('http'); 
-	// httpProxy.createServer(function (req, res, proxy) {
-	// 	console.log('old request url ' + req.url);
- //    	req.url = '/' + req.url.split('/').slice(2).join('/'); // remove the '/api' part
- //    	console.log('new request url ' + req.url);
-	//   	proxy.proxyRequest(req, res, {
-	//     	host: 'secret-meadow-5568.herokuapp.com'
-	//   	});
-	// }).listen(9009);
- 
-
 	app.use(express.static(__dirname + '/dist'));
-	// // app.use(express.static(__dirname + '/.tmp/'));
 
     proxy.on('error', function (err, req, res) {
     	console.log('error' + err);
@@ -32,14 +20,6 @@
     	console.log('new request url ' + req.url);
 	    req.headers.host = 'secret-meadow-5568.herokuapp.com';
         return proxy.proxyRequest(req, res, { target: 'https://secret-meadow-5568.herokuapp.com' });
-        // return proxy.proxyRequest(req, res, { target: 'http://190.16.207.199:3000' });
 	});
 	app.listen(process.env.PORT || 9009);
-	 
-	// http.createServer(function(req, res) {
-	// 	// req.headers.host = 'api-target.herokuapp.com';
-	// 	proxy.web(req, res, { target: 'http://secret-meadow-5568.herokuapp.com' });
-	// }).listen(process.env.PORT || 5000);
-
-
 }());
